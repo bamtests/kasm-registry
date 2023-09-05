e126579341e2e8ea9af19ae52b47c326f8fef78b
bamtests <17178592+bamtests@users.noreply.github.com>
2023-09-04 20:25:10 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'bamtests kasm registry',
+    description: '3rd party registry adding some common applications',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://bamtests.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/bamtests/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
